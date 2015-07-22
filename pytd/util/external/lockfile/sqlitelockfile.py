520e3b13c648d9f3835269161b352f851c7f5224
sebcourtois <neitsabes974@gmail.com>
2015-07-22 20:19:17 +0200

    one more "PydevCodeAnalysisIgnore" in an external lib

--- pytd/util/external/lockfile/sqlitelockfile.py
+++ pytd/util/external/lockfile/sqlitelockfile.py
@@ -1,3 +1,5 @@
+
+#@PydevCodeAnalysisIgnore
 from __future__ import absolute_import, division
 
 import time
@@ -34,7 +36,7 @@ class SQLiteLockFile(LockBase):
 
         import sqlite3
         self.connection = sqlite3.connect(SQLiteLockFile.testdb)
-        
+
         c = self.connection.cursor()
         try:
             c.execute("create table locks"
@@ -97,7 +99,7 @@ class SQLiteLockFile(LockBase):
                 if len(rows) == 1:
                     # We're the locker, so go home.
                     return
-                    
+
             # Maybe we should wait a bit longer.
             if timeout is not None and time.time() > end_time:
                 if timeout > 0:
@@ -130,7 +132,7 @@ class SQLiteLockFile(LockBase):
                        "  where lock_file = ?",
                        (self.lock_file,))
         return cursor.fetchone()[0]
-        
+
     def is_locked(self):
         cursor = self.connection.cursor()
         cursor.execute("select * from locks"
